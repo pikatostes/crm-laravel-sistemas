f741d0293984310e8a18d6d2ce76f3c7fe74f31c
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2026-02-02 21:37:44 +0100

    Proveedor soft delete

--- database/migrations/2026_02_02_152753_create_proveedors_table.php
+++ database/migrations/2026_02_02_152753_create_proveedors_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('empresa');
             $table->string('direccion')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
